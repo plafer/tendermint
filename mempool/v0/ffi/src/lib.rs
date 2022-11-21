fa1753f2235397998a7b6e6567de82cf67830f8c
Philippe Laferriere <plafer@protonmail.com>
2022-11-21 16:11:10 -0500

    docstring

--- mempool/v0/ffi/src/lib.rs
+++ mempool/v0/ffi/src/lib.rs
@@ -1,10 +1,11 @@
-pub mod ffi;
 ///! Rust implementation of the CList mempool
 ///! The current implementation only supports one mempool instantiated.
 ///! However, the handle-based API is designed to support arbitrarily many.
 ///! Every function exposed is expected to be accessed be 1 thread at a time.
 ///! In other words, the go code is expected to guard the access to the FFI
 ///! with a mutex.
+
 mod mempool;
+pub mod ffi;
 
 pub use ffi::*;
